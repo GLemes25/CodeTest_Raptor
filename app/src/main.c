4896bacc96513e9a8ead16cecf18a308fbf7956b
GLemes25 <gabriellemes924@gmail.com>
2025-03-23 14:22:36 -0400

    crição do projeto com a mensagem "Hello Raptor"

---
+++ app/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+
+int main(void)
+{
+	
+	printk("Zephyr Example Application ");
+
+	return 0;
+}
+
